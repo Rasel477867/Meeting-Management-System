45f02dc5d0ef361c291326145a8e73c5a136ccf7
“Rasel477867” <raselaye3737@gamil.com>
2025-01-29 23:12:05 +0600

    Project Setup

---
+++ MMSWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MMSWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
