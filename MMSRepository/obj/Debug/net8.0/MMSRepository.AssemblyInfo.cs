bcbb22c62bbf47188adf64914bb0843946e66c7d
“Rasel477867” <raselaye3737@gamil.com>
2025-02-01 00:19:45 +0600

    NoRepeat Option Check fixed.

--- MMSRepository/obj/Debug/net8.0/MMSRepository.AssemblyInfo.cs
+++ MMSRepository/obj/Debug/net8.0/MMSRepository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MMSRepository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87c68079ab4cc82eac84352c5aae5e25df4d6daa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9d22a7744c52a4488102d73a9d16510030e8efe")]
 [assembly: System.Reflection.AssemblyProductAttribute("MMSRepository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MMSRepository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MMSService/BookingService.cs
+++ MMSService/BookingService.cs
@@ -36,21 +36,85 @@ namespace MMSService
             //No Repeat false
             if (booking.RepetitionOption == RepeatOption.NoRepeat)
             {
-                return ! await existingBookings.AnyAsync(x =>
+                var state=! await existingBookings.AnyAsync(x =>
                     x.BookingDate == booking.BookingDate &&
-                    ((x.StartTime < booking.StartTime && booking.StartTime < x.EndTime) ||
-                     (x.StartTime < booking.EndTime && booking.EndTime < x.EndTime) ||
-                     (x.StartTime == booking.StartTime && x.EndTime == booking.EndTime))); // Check exact same time
+                    ((x.StartTime <= booking.StartTime && booking.StartTime <= x.EndTime) ||
+                     (x.StartTime <= booking.EndTime && booking.EndTime <= x.EndTime))); // Check exact same time
+                if (state == false)
+                {
+                    return false;
+                }
+                else
+                {
+                    var list= existingBookings.ToList();
+                    for(var i=0; i<list.Count; i++){
+                        if (list[i].RepetitionOption == RepeatOption.Daily) {
+                            if (list[i].BookingDate<=booking.BookingDate && booking.BookingDate <= list[i].EndRepeatedDate)
+                            {
+                             
+                                if (list[i].StartTime<=booking.StartTime && booking.StartTime <= list[i].EndTime)
+                                {
+                                    return false;
+                                }
+                                else if (list[i].StartTime <= booking.EndTime && booking.EndTime <= list[i].EndTime)
+                                {
+                                    return false;
+                                }
+                            }
+                        
+                        }
+                        else if (list[i].RepetitionOption == RepeatOption.Weekly)
+                        {
+                            if (list[i].BookingDate <= booking.BookingDate && booking.BookingDate <= list[i].EndRepeatedDate)
+                            {
+                                var inputday = (DaysofworkEnum)(1 << (int)booking.BookingDate.DayOfWeek); // Get bitwise value of the weekday
+                                var existday = list[i].DaysToRepeatedOn;
+                                var ans = ((int?)existday ?? 0) & (int)inputday;
+                                if (ans != 0)
+                                {
+                                    if (list[i].StartTime <= booking.StartTime && booking.StartTime <= list[i].EndTime)
+                                    {
+                                        return false;
+                                    }
+                                    else if (list[i].StartTime <= booking.EndTime && booking.EndTime <= list[i].EndTime)
+                                    {
+                                        return false;
+                                    }
+                                }
+
+
+                            }
+                        }
+
+
+                    }
+                   
+                }
             }
             // Daily Book false
             if (booking.RepetitionOption == RepeatOption.Daily && booking.EndRepeatedDate.HasValue)
             {
-                return ! await existingBookings.AnyAsync(x =>
+                var state = !await existingBookings.AnyAsync(x =>
                     x.RepetitionOption == RepeatOption.Daily &&
                     x.BookingDate <= booking.BookingDate &&
                     x.EndRepeatedDate >= booking.BookingDate &&
                     x.StartTime < booking.EndTime &&
                     x.EndTime > booking.StartTime);
+                if (state == false)
+                {
+                    return false;
+                }
+                else
+                {
+                    var list=existingBookings.ToList();
+                    for(int i=0; i<list.Count; i++)
+                    {
+                        if (list[i].RepetitionOption == RepeatOption.NoRepeat)
+                        {
+
+                        }
+                    }
+                }
             }
 
           // Weekly Option false
